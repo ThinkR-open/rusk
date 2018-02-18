874d3ed31eb83af140958451a25e99c3e56315b1
vincent <vincent@thinkr.fr>
2018-02-18 13:46:07 +0100

    bugreport

--- data-raw/animation.R
+++ data-raw/animation.R
@@ -2,7 +2,7 @@ library(magick)
 
 library(ggplot2)
 img <- image_graph(400, 400, res = 96)
-seq(2,100,by = 1) %>% purrr::map(~draw(table=9,modulo = .x))
+seq(2,200,by = 1) %>% purrr::map(~draw(table=21,modulo = .x))
 
 dev.off()
 animation <- image_animate(img, fps = 10)
